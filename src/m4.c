b68e1eb2ac3c03b9bcb42b9f2dbcd7a2d03eb3b3
tar-mirror <tar@mirror.local>
2017-03-03 03:32:17 +0000

    Snapshot from m4-1.4.4.tar.bz2

--- src/m4.c
+++ src/m4.c
@@ -1,12 +1,12 @@
 /* GNU m4 -- A simple macro processor
 
-   Copyright (C) 1989, 1990, 1991, 1992, 1993, 1994, 2004 Free
+   Copyright (C) 1989, 1990, 1991, 1992, 1993, 1994, 2004, 2005 Free
    Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +15,8 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301  USA
 */
 
 #include "m4.h"
@@ -259,6 +260,7 @@ static const struct option long_options[] =
 int
 main (int argc, char *const *argv, char *const *envp)
 {
+  int retcode = EXIT_SUCCESS;
   macro_definition *head;	/* head of deferred argument list */
   macro_definition *tail;
   macro_definition *new;
@@ -484,6 +486,9 @@ warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\
 	    if (fp == NULL)
 	      {
 		error (0, errno, "%s", argv[optind]);
+		/* Set the status to EXIT_FAILURE, even though we
+		   continue to process files after a missing file.  */
+		retcode = EXIT_FAILURE;
 		continue;
 	      }
 	    else
@@ -506,5 +511,5 @@ warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n\
       undivert_all ();
     }
 
-  exit (EXIT_SUCCESS);
+  exit (retcode);
 }
